06c8a00e368c3857e5845950027d92f3c0af28c7
Evan Lynn <evanlynn.wei@gmail.com>
2022-03-13 22:33:48 +0800

    fix bug

--- dist/popup.js
+++ dist/popup.js
@@ -1,5 +1,7 @@
 // fetch current tabs selected text, then send message to background
 chrome.tabs.executeScript({ code: "window.getSelection().toString()" }, function (selection) {
-    const textareaElement = document.getElementById("row-text")
-    textareaElement.value = selection
+    if (selection != "") {
+        const textareaElement = document.getElementById("row-text")
+        textareaElement.value = selection
+    }
 })
